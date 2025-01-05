9951d686cf206b0c28bcbfd80e84438c7051c66b
NataliaPnk <156689583+NataliaPnk@users.noreply.github.com>
2025-01-05 20:17:58 +0100

    order_details_form

--- telran_project_frontend/src/App.js
+++ telran_project_frontend/src/App.js
@@ -1,6 +1,6 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { useDispatch } from "react-redux";
 import { getAllCategories } from "./requests/categories";
@@ -12,8 +12,12 @@ import ProductsByCategoryPage from "./pages/ProductsByCategoryPage";
 import ProductPage from "./pages/ProductPage";
 import AllSalesPage from "./pages/AllSalesPage";
 import CartPage from "./pages/CartPage";
+import { Context } from "./context";
+//import FeedbackWindow from "./components/FeedbackWindow";
 
 function App() {
+  const [feedbackActive, setFeedbackActive] = useState(true);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -23,19 +27,22 @@ function App() {
 
   return (
     <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/all_categories" element={<AllCategoriesPage />} />
-        <Route
-          path="/categories/:category_name"
-          element={<ProductsByCategoryPage />}
-        />
-        <Route path="/products/:product_id" element={<ProductPage />} />
-        <Route path="/all_sales" element={<AllSalesPage />} />
-        <Route path="/cart" element={<CartPage />} />
-      </Routes>
-      <Footer />
+      <Context.Provider value={{ feedbackActive, setFeedbackActive }}>
+        {/* <FeedbackWindow /> */}
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/all_categories" element={<AllCategoriesPage />} />
+          <Route
+            path="/categories/:category_name"
+            element={<ProductsByCategoryPage />}
+          />
+          <Route path="/products/:product_id" element={<ProductPage />} />
+          <Route path="/all_sales" element={<AllSalesPage />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+        <Footer />
+      </Context.Provider>
     </div>
   );
 }
--- telran_project_frontend/src/pages/CartPage/index.jsx
+++ telran_project_frontend/src/pages/CartPage/index.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import CartContainer from "../../components/CartContainer";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import OrderDetailsForm from "../../components/OrderDetailsForm";
 
-export default function CartPage() {
+export default function CartPage({ count }) {
+  const cartState = useSelector((store) => store.cart);
+
   return (
     <div>
-      <CartContainer />
+      {cartState.length === 0 ? (
+        <div>
+          {" "}
+          <p>Looks like you have no items in your basket currently.</p>
+          <button>
+            <Link to="/all_products">Continue Shopping</Link>
+          </button>
+        </div>
+      ) : (
+        <div>
+          <CartContainer />
+          <OrderDetailsForm count={count} />
+        </div>
+      )}
     </div>
   );
 }
