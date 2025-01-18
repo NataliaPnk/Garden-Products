64dc4d7c8451f2a591a70973ccbb9223162216e0
Karyna Rubushkova <rubushkova333@gmail.com>
2025-01-18 16:00:50 +0100

    Task: All Products Page

--- telran_project_frontend/src/store/reducers/productsReducer.js
+++ telran_project_frontend/src/store/reducers/productsReducer.js
@@ -1,6 +1,7 @@
 const LOAD_ALL_PRODUCTS = "LOAD_ALL_PRODUCTS";
 const FILTER_BY_PRICE = 'FILTER_BY_PRICE';
 const SORT_ALL_PRODUCTS = 'SORT_ALL_PRODUCTS';
+const FILTER_DISCOUNTED_PRODUCTS = 'FILTER_DISCOUNTED_PRODUCTS';
 
 export const loadAllProductsAction = (products) => ({
   type: LOAD_ALL_PRODUCTS,
@@ -12,6 +13,8 @@ export const filterByPriceAction = values => ({
 }); 
 
 export const sortAllProductsAction = value => ({type: SORT_ALL_PRODUCTS, payload: value});
+
+export const filterDiscountedProductsAction = value => ({type: FILTER_DISCOUNTED_PRODUCTS, payload: value});
 
 export const productsReducer = (state = [], action) => {
   if (action.type === LOAD_ALL_PRODUCTS) {
@@ -31,7 +34,22 @@ export const productsReducer = (state = [], action) => {
     } else if (action.payload === 'price_desc') {
       return state.slice().sort((a, b) => b.price - a.price);
     } 
-  }
+  } else if (action.type === FILTER_DISCOUNTED_PRODUCTS) {
+    if(action.payload){
+      state.map(el => {
+        if(el.discont_price === null){
+          el.visible = false
+        }
+        return el
+      })
+    } else {
+      state.map(el => {
+        el.visible = true;
+        return el
+      })
+    }
+    return [...state]
+  }   
   return state;
 };
 
